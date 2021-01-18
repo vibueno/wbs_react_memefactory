19bc52e7b27ca4d1d5ac135f3a752fef2e52f7b1
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-18 14:19:52 +0100

    Call to API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [memePic, setMemePic] = useState();
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => setMemePic(response.data.memes[0].url));
+  });
+
+  return (
+    <>
+      <img src={memePic} />
+    </>
+  );
 }
 
 export default App;
