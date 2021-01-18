e1b1b02c11eb1a2c5d5453b28934b787f76033f4
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-18 14:24:26 +0100

    Get random picture

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import "./App.css";
 function App() {
   const [memePic, setMemePic] = useState();
 
+  const errorHandler = (response) => {
+    if (!response.ok) throw Error(response.statusText);
+    else return response;
+  };
+
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
+      .then(errorHandler)
       .then((response) => response.json())
-      .then((response) => setMemePic(response.data.memes[0].url));
-  });
+      .then((response) => {
+        const memes = response.data.memes;
+        const picIndex = Math.round(Math.random() * memes.length - 1);
+        setMemePic(memes[picIndex].url);
+      });
+  }, []);
 
   return (
     <>
